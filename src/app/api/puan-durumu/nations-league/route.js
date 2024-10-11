a613739bc7b50822f5a654693c0ce9bb8d138539
Berkay Guler <berkay4058@gmail.com>
2024-10-11 19:19:59 +0300

    axios settings
    Signed-off-by: Berkay Guler <berkay4058@gmail.com>

--- src/app/api/puan-durumu/nations-league/route.js
+++ src/app/api/puan-durumu/nations-league/route.js
@@ -36,8 +36,11 @@ export async function GET(req) {
 
     const response = await axios.get(url, {
       headers: {
-        "Cache-Control": "no-cache", // Sunucuya yanıtın önbelleğe alınmaması gerektiğini söyler
+        "Cache-Control": "no-cache",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
       },
+      timeout: 10000, // 10 saniye
     });
 
     if (response.status !== 200)
